f8000dae793480652aa03d8fc513f2af97fe127f
Анастасія Бевзюк <anastasiabevzuk1@gmail.com>
2023-09-24 15:37:04 +0300

    Добавьте файлы проекта.

---
+++ TimeLibTestProject/UnitTest1.cs
@@ -0,0 +1,80 @@
+using NUnit.Framework;
+using CSharpTimeLib;
+
+namespace TimeLibTestProject
+{
+    [TestFixture]
+    public class TimeTests
+    {
+        [TestCase(2, 70, 56, 3, 10, 56, "03:10:56")]
+        [TestCase(12, 99, 70, 13, 40, 10, "13:40:10")]
+        [TestCase(14, 15, 10, 14, 15, 10, "14:15:10")]
+        [TestCase(24, 49, 82, 24, 50, 22, "24:50:22")]
+        public void TestToString(int h1, int m1, int s1, int h2, int m2, int s2, string expected)
+        {
+            var time1 = new Time(h1, m1, s1);
+            var time2 = new Time(h2, m2, s2);
+
+            Assert.AreEqual(expected, time1.ToString());
+            Assert.AreEqual(expected, time2.ToString());
+        }
+
+
+        [TestCase(2, 38, 54, 10, 12, 4, 12, 50, 58)]
+        [TestCase(12, 45, 10, 1, 9, 12, 13, 54, 22)]
+        [TestCase(5, 39, 52, 18, 10, 30, 23, 50, 22)]
+        [TestCase(8,20, 18, 10, 50, 38, 19, 10, 56)]
+        public void TestAddTime(int h1,int m1, int s1, int h2, int m2, int s2, int h3, int m3, int s3)
+        {
+            Time time1 = new Time(h1, m1, s1);
+            Time time2 = new Time(h2, m2, s2);
+            Time result = time1.AddTime(time2);
+
+            Assert.AreEqual(h3, result.Hours);
+            Assert.AreEqual(m3, result.Minutes);
+            Assert.AreEqual(s3, result.Seconds);
+
+        }
+
+
+        [TestCase(4, 52, 18, 2, 40, 10, 2, 12, 8)]
+        [TestCase(12, 18, 46, 2, 2, 20, 10, 16, 26)]
+        [TestCase(20, 48, 13, 16, 18, 5, 4, 30, 8)]
+        [TestCase(7, 36, 12, 7, 18, 10, 0, 18, 2)]
+        public void TestSubtractTime(int h1, int m1, int s1, int h2, int m2, int s2, int h3, int m3, int s3)
+        {
+            Time time1 = new Time(h1, m1, s1);
+            Time time2 = new Time(h2, m2, s2);
+            Time result = time1.SubtractTime(time2);
+
+            Assert.AreEqual(h3, result.Hours);
+            Assert.AreEqual(m3, result.Minutes);
+            Assert.AreEqual(s3, result.Seconds);
+        }
+
+        [TestCase(5, 48, 52, 20932)]
+        [TestCase(3, 10, 30, 11430)]
+        [TestCase(10, 1, 10, 36070)]
+        [TestCase(18, 10, 12, 65412)]
+        public void TestToSeconds(int h, int m, int s, int expectedSeconds)
+        {
+            var time = new Time(h, m, s);
+            Assert.AreEqual(expectedSeconds, time.ToSeconds());
+        }
+
+
+        [TestCase(20932, 5, 48, 52)]
+        [TestCase(11430, 3, 10, 30)]
+        [TestCase(36070, 10, 1, 10)]
+        [TestCase(65412, 18, 10, 12)]
+        public void TestFromSeconds(int s1, int h, int m, int s)
+        {
+            var result = Time.FromSeconds(s1);
+
+            Assert.AreEqual(h, result.Hours);
+            Assert.AreEqual(m, result.Minutes);
+            Assert.AreEqual(s, result.Seconds);
+        }
+
+    }
+}
---
+++ csharptimelib/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text;
+
+namespace CSharpTimeLib
+{
+    public class Time
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = Encoding.UTF8;
+
+
+        }
+        private uint seconds;
+
+        public Time()
+        {
+            seconds = 0;
+        }
+
+        public Time(int h, int m, int s)
+        {
+            seconds = (uint)((h * 60 + m) * 60 + s);
+        }
+
+        public Time(string buffer)
+        {
+            string[] tmp = buffer.Split(':');
+
+            if (tmp.Length == 3 && int.TryParse(tmp[0], out int h) && int.TryParse(tmp[1], out int m) && int.TryParse(tmp[2], out int s))
+            {
+                int seconds = (h * 60 + m) * 60 + s;
+                
+            }
+            else
+            {
+                
+            }
+        }
+
+        public int Hours
+        {
+            get
+            {
+                return (int)seconds / 3600;
+            }
+        }
+
+        public int Minutes
+        {
+            get
+            {
+                return (int)seconds / 60 % 60;
+            }
+        }
+
+        public int Seconds
+        {
+            get
+            {
+                return (int)seconds % 60;
+            }
+        }
+
+        public Time AddTime(Time t1)
+        {
+            int totalSeconds = (int)this.seconds + (int)t1.seconds;
+            return new Time(totalSeconds / 3600, (totalSeconds / 60) % 60, totalSeconds % 60);
+        }
+
+        public Time SubtractTime(Time t1)
+        {
+            int totalSeconds = (int)this.seconds - (int)t1.seconds;
+            return new Time(totalSeconds / 3600, (totalSeconds / 60) % 60, totalSeconds % 60);
+        }
+
+        public int ToSeconds()
+        {
+            return (int)this.seconds;
+        }
+
+        public static Time FromSeconds(int totalSeconds)
+        {
+            return new Time(totalSeconds / 3600, (totalSeconds / 60) % 60, totalSeconds % 60);
+        }
+
+        public override string ToString()
+        {
+            uint hours, minutes, sec = seconds % 60;
+            minutes = (seconds / 60) % 60;
+            hours = (seconds / 3600);
+            return $"{hours:D2}:{minutes:D2}:{sec:D2}";
+        }
+    }
+}
